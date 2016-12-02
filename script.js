144e8d705e02fc779f5239f73c47a740c6d51c26
Ali Azam <aliazam@hotmail.co.uk>
2016-12-03 03:02:29 +0500

    more or less completed

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 var arr = 0;
 var imgSlider = document.getElementById('imageSlider');
 
+
 function funcNext(){
     arr++;
     console.log(arr);
-}
-
-function funcPrev(){
-    arr--;
-    console.log(arr);
-}
-
-switch (arr){
+    switch (arr){
     case 0: imgSlider.src = "img/wallpaper1.jpg";
     break;
     case 1: imgSlider.src = "img/wallpaper2.jpg";
@@ -22,4 +16,39 @@ switch (arr){
     break;
     case 4: imgSlider.src = "img/wallpaper5.jpg";
     break;
+    default: arr =0;
+    break;
 }
+}
+
+function funcPrev(){
+    arr--;
+    console.log(arr);
+    switch (arr){
+    case 0: imgSlider.src = "img/wallpaper1.jpg";
+    break;
+    case 1: imgSlider.src = "img/wallpaper2.jpg";
+    break;
+    case 2: imgSlider.src = "img/wallpaper3.jpg";
+    break;
+    case 3: imgSlider.src = "img/wallpaper4.jpg";
+    break;
+    case 4: imgSlider.src = "img/wallpaper5.jpg";
+    break;
+    default: arr = 5;
+    break;
+}
+}
+
+// switch (arr){
+//     case 0: imgSlider.src = "img/wallpaper1.jpg";
+//     break;
+//     case 1: imgSlider.src = "img/wallpaper2.jpg";
+//     break;
+//     case 2: imgSlider.src = "img/wallpaper3.jpg";
+//     break;
+//     case 3: imgSlider.src = "img/wallpaper4.jpg";
+//     break;
+//     case 4: imgSlider.src = "img/wallpaper5.jpg";
+//     break;
+// }
